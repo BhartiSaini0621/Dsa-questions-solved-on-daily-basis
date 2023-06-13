aef3f17182d22a71723e30232c068d591cf185de
BhartiSaini0621 <115688141+BhartiSaini0621@users.noreply.github.com>
2023-06-13 20:54:18 +0530

    Add files via upload

---
+++ matrix/searchRowColSortedMatrix.cpp
@@ -0,0 +1,82 @@
+/* Given a matrix of size n x m, where every row and column is sorted in increasing order, and a number x. Find whether element x is present in the matrix or not.
+
+Example 1:
+
+Input:
+n = 3, m = 3, x = 62
+matrix[][] = {{ 3, 30, 38},
+              {36, 43, 60},
+              {40, 51, 69}}
+Output: 0
+Explanation:
+62 is not present in the matrix, 
+so output is 0.*/
+
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to search a given number in row-column sorted matrix.
+    bool search(vector<vector<int> > matrix, int n, int m, int x) 
+    {
+        // NAIVE APPROACH
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         if(matrix[i][j]==x) return 1;
+        //     }
+        // }
+        
+        // return 0;
+        
+        // EFFICIENT APPROACH
+        int i=0,j=m-1;
+        while(i<n&& j>=0){
+            if(matrix[i][j]==x) return 1;
+            else if(matrix[i][j]>x) j--;
+            else i++;
+        }
+        
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+	int t;
+    cin>>t;
+    while(t--)
+    {
+
+ 		int n, m;
+    	cin>> n>> m;
+        vector<vector<int> > matrix(n);
+            
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].assign(m, 0);
+            for( int j=0; j<m; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+        
+        int x;
+        cin>>x;
+        Solution obj;
+        if (obj.search(matrix,n,m,x)) 
+            cout << "1\n"; 
+        else
+            cout << "0\n"; 
+    }
+	return 0; 
+} 
+
+
+// } Driver Code Ends
